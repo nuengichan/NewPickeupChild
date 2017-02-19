fd3eeade263b29bea4188e0365f3102bdf8691fa
NUENGIG <pk_nueng@hotmail.com>
2017-02-20 02:51:23 +0700

    google Circle school

--- app/src/main/java/com/example/database/MapActivity.java
+++ app/src/main/java/com/example/database/MapActivity.java
@@ -3,6 +3,7 @@ package com.example.database;
 import android.*;
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -10,13 +11,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class MapActivity extends AppCompatActivity implements GoogleMap.OnMyLocationButtonClickListener,OnMapReadyCallback,ActivityCompat.OnRequestPermissionsResultCallback{
+public class MapActivity extends AppCompatActivity implements GoogleMap.OnMyLocationButtonClickListener ,
+        GoogleMap.OnMapClickListener,
+        GoogleMap.OnMarkerClickListener,
+        OnMapReadyCallback,
+        ActivityCompat.OnRequestPermissionsResultCallback{
 
+    private static final LatLng School = new LatLng(13.7626198, 100.6625916 );
+    private Marker mSelectedMarker;
     /**
      * Request code for location permission request.
      *
@@ -32,6 +46,8 @@ public class MapActivity extends AppCompatActivity implements GoogleMap.OnMyLoca
 
     private GoogleMap mMap;
 
+    private Marker add;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +61,44 @@ public class MapActivity extends AppCompatActivity implements GoogleMap.OnMyLoca
     public void onMapReady(GoogleMap map) {
         mMap = map;
 
+        mMap.getUiSettings().setZoomControlsEnabled(false);
+        addMarkersToMap();
+        // Set listener for marker click event.  See the bottom of this class for its behavior.
+        mMap.setOnMarkerClickListener(this);
+
+        // Set listener for map click event.  See the bottom of this class for its behavior.
+        mMap.setOnMapClickListener(this);
+
+
+
+
         mMap.setOnMyLocationButtonClickListener(this);
         enableMyLocation();
+    }
+
+    Circle circle ;
+
+
+    private void addMarkersToMap() {
+
+        add = mMap.addMarker(new MarkerOptions()
+                .position(School)
+                .title("School")
+                .snippet("Population: 2,074,200"));
+
+        circle = drawCircle(new LatLng(13.7626198, 100.6625916));
+    }
+
+    private Circle drawCircle(LatLng latLng) {
+
+        CircleOptions add = new CircleOptions()
+                .center(latLng)
+                .radius(100)
+                .fillColor(0x33FF0000)
+                .strokeColor(Color.BLUE)
+                .strokeWidth(3);
+
+        return mMap.addCircle(add);
     }
 
     /**
@@ -58,9 +110,11 @@ public class MapActivity extends AppCompatActivity implements GoogleMap.OnMyLoca
             // Permission to access the location is missing.
             PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
                     android.Manifest.permission.ACCESS_FINE_LOCATION, true);
+
         } else if (mMap != null) {
             // Access to the location has been granted to the app.
             mMap.setMyLocationEnabled(true);
+
         }
     }
 
@@ -108,4 +162,31 @@ public class MapActivity extends AppCompatActivity implements GoogleMap.OnMyLoca
                 .newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
 
+    @Override
+    public void onMapClick(final LatLng point) {
+        // Any showing info window closes when the map is clicked.
+        // Clear the currently selected marker.
+        mSelectedMarker = null;
+    }
+
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+        // The user has re-tapped on the marker which was already showing an info window.
+        if (marker.equals(mSelectedMarker)) {
+            // The showing info window has already been closed - that's the first thing to happen
+            // when any marker is clicked.
+            // Return true to indicate we have consumed the event and that we do not want the
+            // the default behavior to occur (which is for the camera to move such that the
+            // marker is centered and for the marker's info window to open, if it has one).
+            mSelectedMarker = null;
+            return true;
+        }
+
+        mSelectedMarker = marker;
+
+        // Return false to indicate that we have not consumed the event and that we wish
+        // for the default behavior to occur.
+        return false;
+    }
 }
+
